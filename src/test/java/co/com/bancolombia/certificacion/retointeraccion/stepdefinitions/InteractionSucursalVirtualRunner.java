c4a1c4b3fac720e0e1d043d6dc21043658d990b3
gabrielfva <gabrielfva@hotmail.com>
2021-01-09 18:31:19 -0500

    Third commit - Runner created

---
+++ src/test/java/co/com/bancolombia/certificacion/retointeraccion/stepdefinitions/InteractionSucursalVirtualRunner.java
@@ -0,0 +1,16 @@
+package co.com.bancolombia.certificacion.retointeraccion.stepdefinitions;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/interaction_sucursal_virtual.feature",
+        glue = "co.com.bancolombia.certificacion.retointeraccion.stepdefinitions",
+        snippets = SnippetType.CAMELCASE
+)
+public class InteractionSucursalVirtualRunner {
+
+}
