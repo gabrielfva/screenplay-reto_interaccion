f7ee3c3a8ca23f433979cc4d1973028f60f6909c
gabrielfva <gabrielfva@hotmail.com>
2021-01-09 18:47:13 -0500

    Fifth commit - step definition created

---
+++ src/test/java/co/com/bancolombia/certificacion/retointeraccion/stepdefinitions/InteractionSucursalVirtualStepDefinitions.java
@@ -0,0 +1,25 @@
+package co.com.bancolombia.certificacion.retointeraccion.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class InteractionSucursalVirtualStepDefinitions {
+
+    @Given("^that Gabriel wants to try to enter to the Bancolombia's virtual branch$")
+    public void thatGabrielWantsToTryToEnterToTheBancolombiaSVirtualBranch() {
+        throw new PendingException();
+    }
+
+
+    @When("^he inserts the user (.*) and his numeric password$")
+    public void heInsertsTheUserAndHisNumericPassword(String arg1) {
+        throw new PendingException();
+    }
+
+    @Then("^he should see the message: (.*)$")
+    public void heShouldSeeTheMessage(String arg1) {
+        throw new PendingException();
+    }
+}
